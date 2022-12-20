baca6428e408d50be202d3271568a9a787c7b8f4
mehmetoztokay@gmail.com <mehmetoztokay@gmail.com>
2022-12-20 22:54:17 +0300

    Winston logger library added to the project.

--- v1/src/models/Wallet/index.js
+++ v1/src/models/Wallet/index.js
@@ -1,4 +1,5 @@
 const Mongoose = require('mongoose')
+const logger = require('../../scripts/loggers/Wallet')
 
 const WalletSchema = new Mongoose.Schema(
   {
@@ -10,5 +11,15 @@ const WalletSchema = new Mongoose.Schema(
     versionKey: false
   }
 )
+
+// If save is successful, log the document
+// You can also use the pre hook to log the document before saving
+// You can also add to database
+WalletSchema.post('save', (doc) => {
+  // logger.log({
+  //   level: 'info',
+  //   message: doc
+  // })
+})
 
 module.exports = Mongoose.model('wallet', WalletSchema)
---
+++ v1/src/scripts/loggers/Wallet.js
@@ -0,0 +1,15 @@
+const winston = require('winston')
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'v1/src/logs/projects/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'v1/src/logs/projects/info.log', level: 'info' }),
+    new winston.transports.File({ filename: 'v1/src/logs/projects/combined.log' })
+    // new winston.transports.Console() // Add this line to log to console
+  ]
+})
+
+module.exports = logger
